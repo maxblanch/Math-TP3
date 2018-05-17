356b72b449b06073a03a04c0a47343e82e8ff5f1
Maxime Blanchard <max@MacBook-Pro.local>
2018-05-17 15:22:46 -0400

    First Commit

---
+++ Matrice.java
@@ -0,0 +1,242 @@
+
+public class Matrice {
+
+    public double[][] a;   // le tableau
+    private int m;          // nombre de lignes
+    private int n;          // nombre de colonnes
+
+    public static void main(String[] args) {
+        
+        // AVANT DE FAIRE LES EXERCICES DU TP3, VOUS DEVEZ IMPLEMENTER LES METHODES ET OBTENIR LE BON RESULTAT !
+        // (notes de cours p.153)
+        double[][] b = new double[][]{
+              { 1, 2, -3, -7 },
+              { 2, -3, 5, 18 },
+              { 4, 1, -2, 24 }
+        };
+        
+        Matrice m = new Matrice(b);
+        
+        m.affiche();
+        
+        m.ajouteMultipleLigne(1, 0, -2);
+        m.ajouteMultipleLigne(2, 0, -4);
+        m.ajouteMultipleLigne(2, 1, -1);
+        m.multiplieLigne(1, -1./7.);
+        m.multiplieLigne(2, -1);
+        m.ajouteMultipleLigne(0, 1, -2);
+        m.ajouteMultipleLigne(0, 2, -0.1428571428571428); 
+        m.ajouteMultipleLigne(1, 2, 1.5714285714285714);
+        
+        // etes-vous en mesure d'expliquer la perte de precision dans l'affichage de la reponse finale ??
+        m.affiche();
+    }
+    
+    /**
+     * Cree une matrice m par n
+     */
+    public Matrice(int nbLignes, int nbColonnes) {
+        this.m = nbLignes;
+        this.n = nbColonnes;
+        this.a = new double[m][n];
+    }
+    
+    /**
+     * Cree une matrice carree n par n
+     */
+    public Matrice(int nbLignes) {
+        this.m = nbLignes;
+        this.n = nbLignes;
+        this.a = new double[m][n];
+    }
+    
+    public Matrice(double[][] b) {
+        m = b.length;
+        n = b[0].length;
+        a = new double[m][n];
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                a[i][j] = b[i][j];
+            }
+        }
+    }
+    
+    /**
+     * Copie une matrice b
+     */
+    public Matrice(Matrice b) {
+        this.a = new double[b.a.length][];
+        for(int i = 0; i < b.a.length; i++) {
+            this.a[i] = b.a[i].clone();
+        }
+    }
+    
+    public void affiche() {
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(a[i][j] + "\t");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+    
+    public int getNbLignes() {
+        return m;
+    }
+
+    public int getNbColonnes() {
+        return n;
+    }
+    
+    public double getElement(int ligne, int colonne) {
+        return a[ligne][colonne];
+    }
+
+    public void setElement(double e, int ligne, int colonne) {
+        a[ligne][colonne] = e;
+    }
+    
+    public boolean estCarree() {
+        return this.getNbLignes() == this.getNbColonnes();
+    }
+
+    public boolean estTriangulaireInferieure() {
+        if(!this.estCarree()) return false;
+        boolean estTrianglulaireInferieure = false;
+        return estTrianglulaireInferieure;
+    }
+    
+    public boolean estTriangulaireSuperieure() {
+        // TODO
+        return true;
+    }
+    
+    public boolean estDiagonale() {
+        // TODO
+        return true;
+    }
+
+    public boolean estScalaire() {
+        // TODO
+        return true;
+    }
+    
+    public boolean estNulle() {
+        // TODO
+        return true;
+    }
+
+    public boolean estLigne() {
+        // TODO
+        return true;
+    }
+    
+    public boolean estColonne() {
+        // TODO
+        return true;
+    }
+    
+    public boolean estSymetrique() {
+        // TODO
+        return true;
+    }
+    
+    /**
+     * Operation elementaire sur les lignes.
+     * Permute la ligne i de la matrice avec la ligne j de la matrice.
+     * L_i <-> L_j
+     */
+    public void permuteLigne(int i, int j) {
+        // TODO
+    }
+
+    /**
+     * Operation elementaire sur les lignes.
+     * Multiplie la ligne i par le facteur d.
+     * L_i <- d * L_i
+     */
+    public void multiplieLigne(int i, double d) {
+        // TODO
+    }
+
+    /**
+     * Operation elementaire sur les lignes.
+     * Ajoute a une ligne un multiple d'une autre ligne.
+     * L_i <- L_i + d * L_j
+     */
+    public void ajouteMultipleLigne(int i, int j, double d) {
+        // TODO
+    }
+
+    /**
+     * Construit une matrice identite n par n
+     */
+    public Matrice identite(int n) {
+        // TODO
+        return null;
+    }
+    
+    /**
+     * Construit une matrice scalaire n par n ayant le nombre e sur la diagonale.
+     */
+    public Matrice scalaire(double e, int n) {
+        // TODO
+        return null;
+    }
+    
+    /**
+     * Retourne la matrice transposee de la matrice passee en argument
+     */
+    public Matrice transpose(Matrice mat) {
+        // TODO
+        return mat;
+    }
+    
+    /**
+     * Multiplie tous les termes de la matrice par le scalaire k. 
+     * Retourne la matrice obtenue (la matrice de depart est inchangee : une nouvelle matrice est creee)
+     */
+    public Matrice multiplieScalaire(double k) {
+        // TODO
+        return null;
+    }
+    
+    /**
+     * Additionne une matrice a la matrice.
+     * Retourne la matrice somme (la matrice de depart est inchangee : une nouvelle matrice est creee)
+     */
+    public Matrice additionMatrices(Matrice b) {
+        // TODO
+        return null;
+    }
+    
+    /**
+     * Multiplie une matrice a la matrice.
+     * Retourne la matrice produit (la matrice de depart est inchangee : une nouvelle matrice est creee)
+     */
+    public Matrice multiplieMatrices(Matrice b) {
+        // TODO
+        return null;
+    }
+    
+    public void question1() {
+        // TODO
+    }
+
+    public void question2() {
+        // TODO
+    }
+
+    public void question3() {
+        // TODO
+    }
+
+    public void question4() {
+        // TODO
+    }
+    
+}
+
---
+++ MatriceTests.java
@@ -0,0 +1,153 @@
+
+
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * The test class MatriceTests.
+ *
+ * @author  (your name)
+ * @version (a version number or a date)
+ */
+public class MatriceTests
+{
+    @Test
+    public void GIVEN_newInstance_WHEN_creatingMatrixWithSpeceficDimensions_THEN_EmptyMatrixIsCreated() {
+        //Arrange
+        int nbLignes = 3;
+        int nbColonnes = 4;
+        
+        //Act
+        Matrice matrixA = new Matrice(nbLignes, nbColonnes);
+        
+        //Assert
+        final double[][] EXPECTED_MATRIX = new double[nbLignes][nbColonnes];
+        final double[][] ACTUAL_MATRIX = matrixA.a;
+        assertEquals(EXPECTED_MATRIX, ACTUAL_MATRIX);
+    }
+    
+    @Test
+    public void GIVEN_newInstance_WHEN_creatingMatrixWithOneNumber_THEN_EmptySquareMatrixIsCreated() {
+        //Arrange
+        int nbLignes = 3;
+        
+        //Act
+        Matrice matrixA = new Matrice(nbLignes);
+        
+        //Assert
+        final double[][] EXPECTED_MATRIX = new double[nbLignes][nbLignes];
+        final double[][] ACTUAL_MATRIX = matrixA.a;
+        assertEquals(EXPECTED_MATRIX, ACTUAL_MATRIX);
+    }
+    
+    @Test
+    public void GIVEN_newInstance_WHEN_creatingMatrixFromAnotherMatrix_THEN_CopyOfTheMatrixIsCreated() {
+        //Arrange
+        double[][] b = new double[][]{
+              { 1, 2, -3, -7 },
+              { 2, -3, 5, 18 },
+              { 4, 1, -2, 24 }
+        };
+        Matrice matriceB = new Matrice(b);
+        
+        //Act
+        Matrice matrixCopy = new Matrice(matriceB);
+        
+        //Assert
+        final double[][] EXPECTED_MATRIX = b;
+        final double[][] ACTUAL_MATRIX = matrixCopy.a;
+        assertEquals(EXPECTED_MATRIX, ACTUAL_MATRIX);
+    }
+    
+    @Test
+    public void GIVEN_newInstance_WHEN_creatingMatrixFromAnotherMatrix_THEN_ExpectDifferentAdressInMemory() {
+        //Arrange
+        double[][] b = new double[][]{
+              { 1, 2, -3, -7 },
+              { 2, -3, 5, 18 },
+              { 4, 1, -2, 24 }
+        };
+        Matrice matriceB = new Matrice(b);
+        
+        //Act
+        Matrice matrixCopy = new Matrice(matriceB);
+        
+        //Assert
+        final double[][] EXPECTED_MATRIX = b;
+        final double[][] ACTUAL_MATRIX = matrixCopy.a;
+        assertNotSame(EXPECTED_MATRIX, ACTUAL_MATRIX);
+    }
+    
+    @Test
+    public void GIVEN_Matrix_WHEN_MatrixIsSquare_THEN_TestSquareMatrixReturnsTrue() {
+        //Arrange
+        int nbLignes = 2;
+        int nbColonnes = 2;
+        Matrice matriceB = new Matrice(nbLignes, nbColonnes);
+        
+        //Act
+        boolean isSquareMatrix = matriceB.estCarree();
+        
+        //Assert
+        final boolean EXPECTED_ANSWER = true;
+        final boolean ACTUAL_ANSWER = isSquareMatrix;
+        assertEquals(EXPECTED_ANSWER, ACTUAL_ANSWER);
+    }
+    
+    @Test
+    public void GIVEN_Matrix_WHEN_MatrixIsNotSquare_THEN_TestSquareMatrixReturnsFalse() {
+        //Arrange
+        int nbLignes = 2;
+        int nbColonnes = 3;
+        Matrice matriceB = new Matrice(nbLignes, nbColonnes);
+        
+        //Act
+        boolean isSquareMatrix = matriceB.estCarree();
+        
+        //Assert
+        final boolean EXPECTED_ANSWER = false;
+        final boolean ACTUAL_ANSWER = isSquareMatrix;
+        assertEquals(EXPECTED_ANSWER, ACTUAL_ANSWER);
+    }
+    
+    @Test
+    public void GIVEN_Matrix_WHEN_MatriceEstTriangulaireInférieure_THEN_estTriangulaireInférieureReturnsTrue() {
+        //Arrange
+        double[][] b = new double[][]{
+              { 1, 0, 0 },
+              { 2, -3, 0 },
+              { 2, 2, -2 }
+        };
+        Matrice matriceB = new Matrice(b);
+        
+        //Act
+        boolean isMatriceInferieure = matriceB.estTriangulaireInferieure();
+        
+        //Assert
+        final boolean EXPECTED_ANSWER = true;
+        final boolean ACTUAL_ANSWER = isMatriceInferieure;
+        assertEquals(EXPECTED_ANSWER, ACTUAL_ANSWER);
+    }
+    
+    @Test
+    public void GIVEN_Matrix_WHEN_MatriceNEstPasTriangulaireInférieure_THEN_estTriangulaireInférieureReturnsFalse() {
+        //Arrange
+        double[][] b = new double[][]{
+              { 1, 0, 0, 0 },
+              { 2, 1, 0, 0 },
+              { 2, 2, 1, 0 },
+              { 2, 2, 2, 1 }
+        };
+        Matrice matriceB = new Matrice(b);
+        
+        //Act
+        boolean isMatriceInferieure = matriceB.estTriangulaireInferieure();
+        
+        //Assert
+        final boolean EXPECTED_ANSWER = true;
+        final boolean ACTUAL_ANSWER = isMatriceInferieure;
+        assertEquals(EXPECTED_ANSWER, ACTUAL_ANSWER);
+    }
+}
